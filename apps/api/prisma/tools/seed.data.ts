eb322cff6e06d95c7fec4be58fc216da9bcf61f4
Vivian Lin <linvivian61@gmail.com>
2023-09-26 15:29:55 -0400

    feat(api): add prisma schema and data

--- apps/api/prisma/tools/seed.data.ts
+++ apps/api/prisma/tools/seed.data.ts
@@ -1,15 +1,88 @@
-import { User } from 'libs/generated-db-types';
+import {
+  User,
+  Book,
+  UserBook,
+  Shelf,
+  ShelfEntry,
+} from 'libs/generated-db-types'; // Import your Prisma models here
+import { faker } from '@faker-js/faker';
 
-export const USERS_SEED_DATA: User[] = [
-  {
-    id: '1',
-    email: 'admin@bookcue.com',
-    username: 'Administrator Bookcue',
-    hashedPassword:
-      '$2b$10$kRW9o24vrKjquuKVXLSeoOwOLjSfByooHwSp3BnIyjUBSVM2aJv8S',
-    hashedRefreshToken:
-      'asdfasdfkRW9o24vrKjquuKVXLSeoOwOLjSfByooHwSp3BnIyjUBSVM2aJv8S',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
+// Function to create a random user
+export function createRandomUser(): User {
+  return {
+    id: faker.string.uuid(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    hashedPassword: faker.internet.password(),
+    hashedRefreshToken: faker.string.uuid(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    // userBooks: userBooks,
+    // shelves: shelves,
+  };
+}
+
+// Function to create a random book
+export function createRandomBook(): Book {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.words(),
+    author: faker.person.fullName(),
+    pubDate: faker.date.past().toISOString(),
+    publisher: faker.company.name(),
+    coverImage: faker.image.url(),
+    description: faker.lorem.sentence(),
+    pageCount: faker.number.int.toString(),
+    isbn: faker.string.uuid(),
+    category: faker.word.noun(),
+  };
+}
+
+// Function to create a random user book
+export function createRandomUserBook(users: User[], books: Book[]): UserBook {
+  const user = faker.helpers.arrayElement(users);
+  const book = faker.helpers.arrayElement(books);
+
+  return {
+    id: faker.string.uuid(),
+    userId: user.id,
+    bookId: book.id,
+    status: faker.helpers.arrayElement(['Reading', 'Completed']),
+    rating: faker.number.int({ min: 1, max: 5 }),
+    dateStarted: faker.date.past().toISOString(),
+    dateFinished: faker.date.recent().toISOString(),
+    user: user,
+    book: book,
+  };
+}
+
+// Function to create a random shelf
+export function createRandomShelf(users: User[]): Shelf {
+  const user = faker.helpers.arrayElement(users);
+
+  return {
+    id: faker.string.uuid(),
+    shelfName: faker.word.noun(),
+    shelfDescription: faker.lorem.sentence(),
+    userId: user.id,
+    dateTime: faker.date.recent().toISOString(),
+  };
+}
+
+// Function to create a random shelf entry
+export function createRandomShelfEntry(
+  shelves: Shelf[],
+  userBooks: UserBook[],
+): ShelfEntry {
+  const shelf = faker.helpers.arrayElement(shelves);
+  const userBook = faker.helpers.arrayElement(userBooks);
+
+  return {
+    id: faker.datatype.uuid(),
+    shelfId: shelf.id,
+    userBookId: userBook.id,
+    dateTime: faker.date.recent().toISOString(),
+    shelf: shelf,
+    userBook: userBook,
+  };
+}
--- apps/api/prisma/tools/seed.ts
+++ apps/api/prisma/tools/seed.ts
@@ -1,14 +1,91 @@
 import { PrismaClient } from '@prisma/client';
-import { USERS_SEED_DATA } from './seed.data';
+import {
+  createRandomUser,
+  createRandomBook,
+  createRandomUserBook,
+  createRandomShelf,
+} from './seed.data'; // Import your fake data generation functions
+
 const prisma = new PrismaClient();
 
 async function main() {
+  // Generate fake data
+  const users = Array.from({ length: 5 }, createRandomUser);
+  const books = Array.from({ length: 10 }, createRandomBook);
+  const userBooks = Array.from({ length: 20 }, () =>
+    createRandomUserBook(users, books),
+  );
+  const shelves = Array.from({ length: 5 }, () => createRandomShelf(users));
+
   await Promise.all(
-    USERS_SEED_DATA.map((user) => {
-      return prisma.user.upsert({
-        where: { id: user.id },
-        update: user,
-        create: user,
+    users.map((user) => {
+      return prisma.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          hashedPassword: user.hashedPassword,
+          hashedRefreshToken: user.hashedRefreshToken,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+        },
+      });
+    }),
+  );
+
+  await Promise.all(
+    books.map((book) => {
+      return prisma.book.create({
+        data: {
+          id: book.id,
+          title: book.title,
+          author: book.author,
+          pubDate: book.pubDate,
+          publisher: book.publisher,
+          coverImage: book.coverImage,
+          description: book.description,
+          pageCount: book.pageCount,
+          isbn: book.isbn,
+          category: book.category,
+        },
+      });
+    }),
+  );
+
+  // Assuming you have an array of userBooks to create
+
+  await Promise.all(
+    userBooks.map(async (userBook) => {
+      const { id, userId, bookId, status, rating, dateStarted, dateFinished } =
+        userBook;
+      // You can access user and book like userBook.user and userBook.book
+      // Create a UserBook record
+      await prisma.userBook.create({
+        data: {
+          id,
+          userId,
+          bookId,
+          status,
+          rating,
+          dateStarted,
+          dateFinished,
+        },
+      });
+    }),
+  );
+  await Promise.all(
+    shelves.map(async (shelf) => {
+      const { id, shelfName, shelfDescription, userId, dateTime } = shelf;
+      // You can access user and book like userBook.user and userBook.book
+      // Create a UserBook record
+      await prisma.shelf.create({
+        data: {
+          id,
+          shelfName,
+          shelfDescription,
+          userId,
+          dateTime,
+        },
       });
     }),
   );
