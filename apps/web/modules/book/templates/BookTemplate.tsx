38dfb674e4511dcdfb0b89178a6fd9ddafcd5777
Vivian Lin <linvivian61@gmail.com>
2023-10-30 13:35:19 -0400

    book module

---
+++ apps/web/modules/book/templates/BookTemplate.tsx
@@ -0,0 +1,56 @@
+import { UserBookQuery, UserBookDocument, UserBook } from "@/graphql/graphql";
+import { dm_sefif_display } from "@/lib/fonts";
+import { formatDate, cn } from "@/lib/utils";
+import { BookData } from "@/types/interfaces";
+import ActionsPanel from "../components/ActionsPanel";
+import BookDetails from "../components/BookDetails";
+import Image from "next/image";
+interface BookTemplateProps {
+  book: BookData;
+  userBook: UserBookQuery["userBook"];
+}
+
+export default function BookTemplate({ book, userBook }: BookTemplateProps) {
+  return (
+    <>
+      <div className="grid w-full grid-cols-5 gap-2 ">
+        <section className="p-4 hidden xl:block xl:col-span-1">
+          <Image
+            width={184}
+            height={277}
+            src={book.image}
+            className="max-w-none w-[fill-available] rounded-lg"
+            alt="Picture of the author"
+          />
+          <div className="mt-3 text-[12px] font-light">
+            <div>{book.pageNum} Pages</div>
+            <div>First published {formatDate(book.date)}</div>
+          </div>
+        </section>
+        <section className=" p-4 col-span-5 xl:col-span-4 grid gap-2">
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <h1
+                className={cn(
+                  dm_sefif_display.className,
+                  "text-4xl/[1.25] mr-4"
+                )}
+              >
+                {book.title}
+              </h1>
+            </div>
+            <div className="inline text-base">by {book.author}</div>
+            <div className="grid grid-cols-6 gap-2">
+              <section className="flex flex-col gap-2 col-span-4 pr-4">
+                <BookDetails processedBook={book} />
+              </section>
+              <section className="col-span-2">
+                <ActionsPanel book={book} bookStatus={userBook?.status} />
+              </section>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
