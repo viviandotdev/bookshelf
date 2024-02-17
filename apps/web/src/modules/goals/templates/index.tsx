c9736a8d7bb64f6a1df545231f6f17da60653673
Vivian Lin <linvivian61@gmail.com>
2024-02-17 14:57:04 -0500

    create goals page

---
+++ apps/web/src/modules/goals/templates/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface GoalsTemplateProps {
+
+}
+
+export const GoalsTemplate: React.FC<GoalsTemplateProps> = ({ }) => {
+    return (
+        <div>
+            GOALS
+
+        </div>
+    );
+}
+export default GoalsTemplate
