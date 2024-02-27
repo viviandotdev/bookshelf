a193738d2f05d18c7ecbdd94b8dd250dc56049bd
Vivian Lin <linvivian61@gmail.com>
2024-02-27 18:38:03 -0500

    fix home route

--- apps/web/src/modules/layout/components/nav-link.tsx
+++ apps/web/src/modules/layout/components/nav-link.tsx
@@ -2,43 +2,51 @@
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import {
-  useSelectedLayoutSegment,
-  useSelectedLayoutSegments,
+    useSelectedLayoutSegment,
+    useSelectedLayoutSegments,
 } from 'next/navigation';
 import React from 'react';
 
 interface NavLinkProps {
-  href: string;
-  children: React.ReactNode;
-  type?: 'underlined' | 'default'; // Make type optional using `?`
+    href: string;
+    children: React.ReactNode;
+    type?: 'underlined' | 'default'; // Make type optional using `?`
 }
 
 export const NavLink: React.FC<NavLinkProps> = ({
-  href,
-  children,
-  type = 'default',
+    href,
+    children,
+    type = 'default',
 }) => {
-  const segments = useSelectedLayoutSegments();
-  const active = href.startsWith(`/${segments.join('/')}`);
-  let activeStyle;
-  if (type === 'underlined') {
-    activeStyle = active
-      ? 'underline-distance"'
-      : 'text-gray-400 text-xs font-normal';
-  } else {
-    activeStyle = active ? 'bg-beige-100 py-2 px-3 rounded-xl' : '';
-  }
-  return (
-    <Link
-      className={cn(
-        'hover:text-foreground/80 flex items-center font-medium text-beige-700 transition-colors sm:text-sm',
-        activeStyle
-        // item.disabled && "cursor-not-allowed opacity-80"
-      )}
-      href={href}
-    >
-      {children}
-    </Link>
-  );
+    const segments = useSelectedLayoutSegments();
+    console.log(segments, href)
+    let active = href.startsWith(`/${segments.join('/')}`);
+    if (segments.length === 0) {
+        if (href !== '/') {
+            active = false;
+        }
+
+    }
+    let activeStyle;
+    if (type === 'underlined') {
+        activeStyle = active
+            ? 'underline-distance"'
+            : 'text-gray-400 text-xs font-normal';
+    } else {
+        activeStyle = active ? 'bg-beige-100 py-2 px-3 rounded-xl' : '';
+    }
+    console.log(segments, active)
+    return (
+        <Link
+            className={cn(
+                'hover:text-foreground/80 flex items-center font-medium text-beige-700 transition-colors sm:text-sm',
+                activeStyle
+                // item.disabled && "cursor-not-allowed opacity-80"
+            )}
+            href={href}
+        >
+            {children}
+        </Link>
+    );
 };
 export default NavLink;
