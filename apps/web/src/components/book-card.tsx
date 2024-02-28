e2920d329d7c4ddb41fb3a3bfdfa0dc81c375bf2
Vivian Lin <linvivian61@gmail.com>
2024-02-28 07:58:12 -0500

    fix search results page

--- apps/web/src/components/book-card.tsx
+++ apps/web/src/components/book-card.tsx
@@ -14,124 +14,124 @@ import Link from 'next/link';
 const BookCardContext = createContext<{ book: BookData } | null>(null);
 
 export function useBookCardContext() {
-  const context = useContext(BookCardContext);
-  if (!context) {
-    throw new Error(
-      'BookCard.* component must be rendered as child of BookCard component'
-    );
-  }
-  return context;
+    const context = useContext(BookCardContext);
+    if (!context) {
+        throw new Error(
+            'BookCard.* component must be rendered as child of BookCard component'
+        );
+    }
+    return context;
 }
 
 export default BookCardContext;
 
 // Book Header Component
 export function BookShelves() {
-  const { book } = useBookCardContext();
-  const shelves = book.categories;
-  return (
-    <div>
-      {shelves.map((shelf: string, index: number) => (
-        // <beige-100Button label={shelf} />
-        <button
-          key={index}
-          className={cn(
-            buttonVariants({ variant: 'tag', size: 'xs' }),
-            'mb-1 mr-1'
-          )}
-          disabled={true}
-        >
-          {shelf}
-        </button>
-      ))}
-    </div>
-  );
+    const { book } = useBookCardContext();
+    const shelves = book.categories;
+    return (
+        <div>
+            {shelves.map((shelf: string, index: number) => (
+                // <beige-100Button label={shelf} />
+                <button
+                    key={index}
+                    className={cn(
+                        buttonVariants({ variant: 'pill', size: 'xs' }),
+                        'mb-1 mr-1'
+                    )}
+                    disabled={true}
+                >
+                    {shelf}
+                </button>
+            ))}
+        </div>
+    );
 }
 
 export function BookInfo() {
-  return (
-    <div className='flex w-max items-center text-xs font-medium'>
-      {/* <div>Book Year</div> */}
-      <div>Published 2018</div>
-      <Dot />
-      <div>Avg Rating 4.8</div>
-    </div>
-  );
+    return (
+        <div className='flex w-max items-center text-xs font-medium'>
+            {/* <div>Book Year</div> */}
+            <div>Published 2018</div>
+            <Dot />
+            <div>Avg Rating 4.8</div>
+        </div>
+    );
 }
 
 interface BookContentProps {
-  image?: React.ReactNode;
-  shelves?: React.ReactNode;
-  info?: React.ReactNode;
+    image?: React.ReactNode;
+    shelves?: React.ReactNode;
+    info?: React.ReactNode;
 }
 
 export function BookContent({ image, shelves, info }: BookContentProps) {
-  const { book } = useBookCardContext();
-  return (
-    <div className='flex items-start space-x-4 rounded-md'>
-      <BookCover src={book.coverImage} size={'sm'} />
-      <div className='flex flex-col items-start justify-center gap-1'>
-        <CardTitle className='text-base leading-tight '>{book.title}</CardTitle>
-        <CardDescription className='line-clamp-1 text-sm'>
-          {book.author}
-        </CardDescription>
-        {shelves}
-        {info}
-      </div>
-    </div>
-  );
+    const { book } = useBookCardContext();
+    return (
+        <div className='flex items-start space-x-4 rounded-md'>
+            <BookCover src={book.coverImage} size={'sm'} />
+            <div className='flex flex-col items-start justify-center gap-1'>
+                <CardTitle className='text-base leading-tight '>{book.title}</CardTitle>
+                <CardDescription className='line-clamp-1 text-sm'>
+                    {book.author}
+                </CardDescription>
+                {shelves}
+                {info}
+            </div>
+        </div>
+    );
 }
 
 // Book Actions Component
 export function BookActions({
-  buttons,
-  rating,
+    buttons,
+    rating,
 }: {
-  buttons: React.ReactNode[];
-  rating?: React.ReactNode;
+    buttons: React.ReactNode[];
+    rating?: React.ReactNode;
 }) {
-  return (
-    <>
-      {rating}
-      <div className='flex flex-grow justify-end gap-2'>
-        {buttons.map((button, index) => (
-          <div key={index}>{button}</div>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            {rating}
+            <div className='flex flex-grow justify-end gap-2'>
+                {buttons.map((button, index) => (
+                    <div key={index}>{button}</div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 interface BookCardProps {
-  book: BookData;
-  content?: React.ReactNode;
-  actions?: React.ReactNode;
+    book: BookData;
+    content?: React.ReactNode;
+    actions?: React.ReactNode;
 }
 
 export function BookCard({ content, actions, book }: BookCardProps) {
-  const linkRef = useRef<HTMLAnchorElement>(null);
-  return (
-    <BookCardContext.Provider value={{ book }}>
-      <div>
-        <Card
-          onClick={() => {
-            if (linkRef.current) {
-              linkRef.current.click();
-            }
-          }}
-          className={cn(
-            'cursor-pointer overflow-hidden border-none p-0 shadow-none'
-          )}
-        >
-          <CardContent className='flex justify-between gap-4 p-4'>
-            {content}
-            <div>{actions}</div>
-          </CardContent>
-        </Card>
-      </div>
-      <Link ref={linkRef} href={`/book/${book?.id}`} className='hidden'></Link>
-    </BookCardContext.Provider>
-  );
+    const linkRef = useRef<HTMLAnchorElement>(null);
+    return (
+        <BookCardContext.Provider value={{ book }}>
+            <div>
+                <Card
+                    onClick={() => {
+                        if (linkRef.current) {
+                            linkRef.current.click();
+                        }
+                    }}
+                    className={cn(
+                        'cursor-pointer bg-beige-50 overflow-hidden border-none p-0 shadow-none'
+                    )}
+                >
+                    <CardContent className='flex justify-between gap-4 p-4'>
+                        {content}
+                        <div>{actions}</div>
+                    </CardContent>
+                </Card>
+            </div>
+            <Link ref={linkRef} href={`/book/${book?.id}`} className='hidden'></Link>
+        </BookCardContext.Provider>
+    );
 }
 
 BookCard.BookContent = BookContent;
