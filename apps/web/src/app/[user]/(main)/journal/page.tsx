9281800ff3eb1285b69f76271d0f9b25f4830fd2
Vivian Lin <linvivian61@gmail.com>
2024-02-29 07:22:06 -0500

    fix journal page

--- apps/web/src/app/[user]/(main)/journal/page.tsx
+++ apps/web/src/app/[user]/(main)/journal/page.tsx
@@ -3,9 +3,9 @@
 import JournalTemplate from '@/modules/journal/templates';
 
 export default function JournalPage() {
-  return (
-    <main className='container z-40 mx-auto flex-1 '>
-      <JournalTemplate />;
-    </main>
-  );
+    return (
+        <main className='container z-40 mx-auto flex-1 '>
+            <JournalTemplate />
+        </main>
+    );
 }
--- apps/web/src/modules/journal/templates/index.tsx
+++ apps/web/src/modules/journal/templates/index.tsx
@@ -10,132 +10,132 @@ import { Icons } from '@/components/icons';
 import { dm_sefif_display } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
 
-interface JournalTemplateProps {}
+interface JournalTemplateProps { }
 
-export const JournalTemplate: React.FC<JournalTemplateProps> = ({}) => {
-  const [loadEntries, { data: journalData, fetchMore, networkStatus }] =
-    useJournalEntriesLazyQuery({
-      fetchPolicy: 'cache-and-network',
-      nextFetchPolicy: 'cache-first',
-      notifyOnNetworkStatusChange: true,
-      onError: (error) => {
-        toast({
-          title: error.message,
-          variant: 'destructive',
+export const JournalTemplate: React.FC<JournalTemplateProps> = ({ }) => {
+    const [loadEntries, { data: journalData, fetchMore, networkStatus }] =
+        useJournalEntriesLazyQuery({
+            fetchPolicy: 'cache-and-network',
+            nextFetchPolicy: 'cache-first',
+            notifyOnNetworkStatusChange: true,
+            onError: (error) => {
+                toast({
+                    title: error.message,
+                    variant: 'destructive',
+                });
+            },
+            onCompleted: (data) => {
+                if (data && data.journalEntries && data.journalEntries.length === 0) {
+                    toast({
+                        title: 'No journal entries are here... yet',
+                    });
+                }
+            },
+            errorPolicy: 'all',
         });
-      },
-      onCompleted: (data) => {
-        if (data && data.journalEntries && data.journalEntries.length === 0) {
-          toast({
-            title: 'No journal entries are here... yet',
-          });
-        }
-      },
-      errorPolicy: 'all',
+
+    const journalEntires = journalData?.journalEntries.map((entry) => {
+        const date = new Date(entry.dateRead);
+        return {
+            userBook: entry.userBook,
+            monthYear: [MONTH[date.getMonth()], date.getFullYear()].join(' '),
+            date: date.getDate(),
+            entry: {
+                id: entry.id,
+                title:
+                    (entry.userBook &&
+                        entry.userBook.book &&
+                        entry.userBook.book.title) ||
+                    '',
+                image:
+                    entry.userBook &&
+                    entry.userBook.book &&
+                    entry.userBook.book.coverImage,
+            },
+            pagesRead: entry.pagesRead,
+            progress: {
+                currentPercent: entry.currentPercent,
+                currentPage: entry.currentPage,
+                totalPages: entry.userBook?.book?.pageCount,
+            },
+            notes: entry.readingNotes || '',
+            liked: true,
+            abandoned: entry.userBook?.status === 'Abandoned',
+        };
     });
+    const loading = networkStatus === NetworkStatus.loading;
+    // process journal entries to table data
+    // get all entries from this user
 
-  const journalEntires = journalData?.journalEntries.map((entry) => {
-    const date = new Date(entry.dateRead);
-    return {
-      userBook: entry.userBook,
-      monthYear: [MONTH[date.getMonth()], date.getFullYear()].join(' '),
-      date: date.getDate(),
-      entry: {
-        id: entry.id,
-        title:
-          (entry.userBook &&
-            entry.userBook.book &&
-            entry.userBook.book.title) ||
-          '',
-        image:
-          entry.userBook &&
-          entry.userBook.book &&
-          entry.userBook.book.coverImage,
-      },
-      pagesRead: entry.pagesRead,
-      progress: {
-        currentPercent: entry.currentPercent,
-        currentPage: entry.currentPage,
-        totalPages: entry.userBook?.book?.pageCount,
-      },
-      notes: entry.readingNotes || '',
-      liked: true,
-      abandoned: entry.userBook?.status === 'Abandoned',
-    };
-  });
-  const loading = networkStatus === NetworkStatus.loading;
-  // process journal entries to table data
-  // get all entries from this user
+    useEffect(() => {
+        const loadData = async () => {
+            await loadEntries({
+                variables: {
+                    offset: 0,
+                    limit: BOOKS_PAGE_SIZE,
+                },
+            });
+        };
 
-  useEffect(() => {
-    const loadData = async () => {
-      await loadEntries({
-        variables: {
-          offset: 0,
-          limit: BOOKS_PAGE_SIZE,
-        },
-      });
-    };
-
-    loadData();
-  }, [loadEntries]);
-  return (
-    <>
-      <section className='relative z-0 space-y-6 pb-8 pt-12'>
-        <div className='bg-white'>
-          <h1
-            className={cn(
-              dm_sefif_display.className,
-              'mb-6 text-4xl font-bold text-beige-700'
-            )}
-          >
-            Vivian's Reading Journal
-          </h1>
-          <div className='grid grid-cols-3 gap-4'>
-            <div className='flex flex-col justify-between gap-1 rounded-lg bg-blue-50 p-4'>
-              <div className='flex items-center justify-between'>
-                <p className='text-base text-beige-700'>
-                  Total pages read today
-                </p>
-                <div className='flex items-center align-middle text-sm font-semibold text-green-600'>
-                  <Icons.trendingUp className='mr-1 h-4 w-4 text-green-600' />
-                  +100 (2.5%)
+        loadData();
+    }, [loadEntries]);
+    return (
+        <>
+            <section className='relative z-0 space-y-6 pb-8 pt-12'>
+                <div className='bg-white'>
+                    <h1
+                        className={cn(
+                            dm_sefif_display.className,
+                            'mb-6 text-4xl font-bold text-beige-700'
+                        )}
+                    >
+                        Vivian's Reading Journal
+                    </h1>
+                    <div className='grid grid-cols-3 gap-4'>
+                        <div className='flex flex-col justify-between gap-1 rounded-lg bg-blue-50 p-4'>
+                            <div className='flex items-center justify-between'>
+                                <p className='text-base text-beige-700'>
+                                    Total pages read today
+                                </p>
+                                <div className='flex items-center align-middle text-sm font-semibold text-green-600'>
+                                    <Icons.trendingUp className='mr-1 h-4 w-4 text-green-600' />
+                                    +100 (2.5%)
+                                </div>
+                            </div>
+                            <p className='text-4xl font-semibold text-beige-700'>351 pages</p>
+                            <p className='text-sm text-gray-500'>Compare to 200 yesterday</p>
+                        </div>
+                        <div className='flex flex-col justify-between rounded-lg bg-blue-50 p-4'>
+                            <div className='flex items-center justify-between'>
+                                <p className='text-base text-beige-700'>
+                                    Total pages read today
+                                </p>
+                                <div className='flex items-center align-middle text-sm font-semibold text-green-600'>
+                                    <Icons.trendingUp className='mr-1 h-4 w-4 text-green-600' />
+                                    +100 (2.5%)
+                                </div>
+                            </div>
+                            <p className='text-4xl font-semibold text-beige-700'>351 pages</p>
+                            <p className='text-sm text-gray-500'>Compare to 200 yesterday</p>
+                        </div>
+                        <div className='flex flex-col justify-between rounded-lg bg-blue-50 p-4'>
+                            <div className='flex items-center justify-between'>
+                                <p className='text-base text-beige-700'>
+                                    Total pages read today
+                                </p>
+                                <div className='flex items-center align-middle text-sm font-semibold text-green-600'>
+                                    <Icons.trendingUp className='mr-1 h-4 w-4 text-green-600' />
+                                    +100 (2.5%)
+                                </div>
+                            </div>
+                            <p className='text-4xl font-semibold text-beige-700'>351 pages</p>
+                            <p className='text-sm text-gray-500'>Compare to 200 yesterday</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <p className='text-4xl font-semibold text-beige-700'>351 pages</p>
-              <p className='text-sm text-gray-500'>Compare to 200 yesterday</p>
-            </div>
-            <div className='flex flex-col justify-between rounded-lg bg-blue-50 p-4'>
-              <div className='flex items-center justify-between'>
-                <p className='text-base text-beige-700'>
-                  Total pages read today
-                </p>
-                <div className='flex items-center align-middle text-sm font-semibold text-green-600'>
-                  <Icons.trendingUp className='mr-1 h-4 w-4 text-green-600' />
-                  +100 (2.5%)
-                </div>
-              </div>
-              <p className='text-4xl font-semibold text-beige-700'>351 pages</p>
-              <p className='text-sm text-gray-500'>Compare to 200 yesterday</p>
-            </div>
-            <div className='flex flex-col justify-between rounded-lg bg-blue-50 p-4'>
-              <div className='flex items-center justify-between'>
-                <p className='text-base text-beige-700'>
-                  Total pages read today
-                </p>
-                <div className='flex items-center align-middle text-sm font-semibold text-green-600'>
-                  <Icons.trendingUp className='mr-1 h-4 w-4 text-green-600' />
-                  +100 (2.5%)
-                </div>
-              </div>
-              <p className='text-4xl font-semibold text-beige-700'>351 pages</p>
-              <p className='text-sm text-gray-500'>Compare to 200 yesterday</p>
-            </div>
-          </div>
-        </div>
-        <DataTable data={journalEntires!} columns={columns} loading={loading} />
-      </section>
-    </>
-  );
+                <DataTable data={journalEntires!} columns={columns} loading={loading} />
+            </section>
+        </>
+    );
 };
 export default JournalTemplate;
