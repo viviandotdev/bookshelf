a97b4bc8264fb3e1a1d545e1060b0cd6e48affa5
Vivian Lin <linvivian61@gmail.com>
2024-03-11 13:08:20 -0400

    fix update book order

--- apps/web/src/modules/bookshelves/components/column-container.tsx
+++ apps/web/src/modules/bookshelves/components/column-container.tsx
@@ -7,130 +7,130 @@ import { useUpdateUserBookOrder } from '../mutations/use-update-userbook-order';
 import { reorder } from '../utils';
 
 interface ColumnContainerProps {
-  data: ColumnWithBooks[];
-  setData: React.Dispatch<React.SetStateAction<ColumnWithBooks[]>>;
+    data: ColumnWithBooks[];
+    setData: React.Dispatch<React.SetStateAction<ColumnWithBooks[]>>;
 }
 
 export const ColumnContainer: React.FC<ColumnContainerProps> = ({
-  data,
-  setData,
+    data,
+    setData,
 }) => {
-  const [orderedData, setOrderedData] = useState(data);
-  const { updateUserBookOrder } = useUpdateUserBookOrder();
-  useEffect(() => {
-    setOrderedData(data);
-  }, [data]);
+    const [orderedData, setOrderedData] = useState(data);
+    const { updateUserBookOrder } = useUpdateUserBookOrder();
+    useEffect(() => {
+        setOrderedData(data);
+    }, [data]);
 
-  const onDragEnd = (result: any) => {
-    const { destination, source, type } = result;
-    if (!destination) {
-      return;
-    }
+    const onDragEnd = (result: any) => {
+        const { destination, source, type } = result;
+        if (!destination) {
+            return;
+        }
 
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
-    if (type === 'card') {
-      // Ordered data is a list of lists with cards
-      let newOrderedData = [...orderedData];
-      // Find the list that the card is currently in
-      const sourceList = newOrderedData.find(
-        (list) => list.title === source.droppableId
-      );
-      const destinationList = newOrderedData.find(
-        (list) => list.title === destination.droppableId
-      );
-      if (!sourceList || !destinationList) {
-        return;
-      }
-      if (!sourceList.books || !destinationList.books) {
-        sourceList.books = [];
-      }
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+        if (type === 'card') {
+            // Ordered data is a list of lists with cards
+            let newOrderedData = [...orderedData];
+            // Find the list that the card is currently in
+            const sourceList = newOrderedData.find(
+                (list) => list.title === source.droppableId
+            );
+            const destinationList = newOrderedData.find(
+                (list) => list.title === destination.droppableId
+            );
+            if (!sourceList || !destinationList) {
+                return;
+            }
+            if (!sourceList.books || !destinationList.books) {
+                sourceList.books = [];
+            }
 
-      if (!destinationList.books) {
-        destinationList.books = [];
-      }
-      // Moving cards within the same list
-      if (source.droppableId === destination.droppableId) {
-        const reorderedCards = reorder(
-          sourceList.books,
-          source.index,
-          destination.index
-        );
-        reorderedCards.forEach((book, index) => {
-          book.order = index;
-        });
-        sourceList.books = reorderedCards;
-        setOrderedData(newOrderedData);
-        const items = reorderedCards.map((userBook, index) => {
-          return {
-            id: userBook.book?.id,
-            title: userBook.book?.title,
-            order: index,
-            status: userBook.status,
-          };
-        });
-        updateUserBookOrder({ items });
-      }
-      // Moving cards between lists
-      else {
-        const [movedCard] = sourceList.books.splice(source.index, 1);
-
-        //  Assign the new status to the moved card
-        movedCard.status = destination.droppableId;
-        // Add the card to the destination list
-
-        destinationList.books.splice(destination.index, 0, movedCard);
-        sourceList.books.forEach((book, index) => {
-          book.order = index;
-        });
-
-        destinationList.books.forEach((book, index) => {
-          book.order = index;
-        });
-        setOrderedData(newOrderedData);
-        const items = destinationList.books.map((userBook, index) => {
-          return {
-            id: userBook.book?.id,
-            title: userBook.book?.title,
-            order: index,
-            status: userBook.status,
-          };
-        });
-        updateUserBookOrder({ items });
-      }
-    }
-  };
-
-  return (
-    <div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId='lists' type='list' direction='horizontal'>
-          {(provided) => (
-            <ol
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className='flex justify-between'
-            >
-              {orderedData.map((list, index) => {
-                return (
-                  <ColumnItem
-                    key={list.title}
-                    isScrollable={true}
-                    setData={setData}
-                    data={list}
-                  />
+            if (!destinationList.books) {
+                destinationList.books = [];
+            }
+            // Moving cards within the same list
+            if (source.droppableId === destination.droppableId) {
+                const reorderedCards = reorder(
+                    sourceList.books,
+                    source.index,
+                    destination.index
                 );
-              })}
-              {provided.placeholder}
-            </ol>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </div>
-  );
+                reorderedCards.forEach((book, index) => {
+                    book.order = index;
+                });
+                sourceList.books = reorderedCards;
+                setOrderedData(newOrderedData);
+                const items = reorderedCards.map((userBook, index) => {
+                    return {
+                        id: parseInt(userBook.book?.id),
+                        title: userBook.book?.title,
+                        order: index,
+                        status: userBook.status,
+                    };
+                });
+                updateUserBookOrder({ items });
+            }
+            // Moving cards between lists
+            else {
+                const [movedCard] = sourceList.books.splice(source.index, 1);
+
+                //  Assign the new status to the moved card
+                movedCard.status = destination.droppableId;
+                // Add the card to the destination list
+
+                destinationList.books.splice(destination.index, 0, movedCard);
+                sourceList.books.forEach((book, index) => {
+                    book.order = index;
+                });
+
+                destinationList.books.forEach((book, index) => {
+                    book.order = index;
+                });
+                setOrderedData(newOrderedData);
+                const items = destinationList.books.map((userBook, index) => {
+                    return {
+                        id: parseInt(userBook.book?.id),
+                        title: userBook.book?.title,
+                        order: index,
+                        status: userBook.status,
+                    };
+                });
+                updateUserBookOrder({ items });
+            }
+        }
+    };
+
+    return (
+        <div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId='lists' type='list' direction='horizontal'>
+                    {(provided) => (
+                        <ol
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            className='flex justify-between'
+                        >
+                            {orderedData.map((list, index) => {
+                                return (
+                                    <ColumnItem
+                                        key={list.title}
+                                        isScrollable={true}
+                                        setData={setData}
+                                        data={list}
+                                    />
+                                );
+                            })}
+                            {provided.placeholder}
+                        </ol>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </div>
+    );
 };
 export default ColumnContainer;
--- apps/web/src/modules/bookshelves/mutations/use-update-userbook-order.tsx
+++ apps/web/src/modules/bookshelves/mutations/use-update-userbook-order.tsx
@@ -1,66 +1,66 @@
 import { UserBook, useUpdateUserBookOrderMutation } from '@/graphql/graphql';
 import { toast } from '@/hooks/use-toast';
 import {
-  ActionState,
-  UseActionOptions,
-  createSafeAction,
+    ActionState,
+    UseActionOptions,
+    createSafeAction,
 } from '@/lib/create-safe-action';
 import { useState } from 'react';
 import { z } from 'zod';
 
 export const UpdateUserBookOrderInput = z.object({
-  items: z.array(
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      order: z.number(),
-      status: z.string(),
-    })
-  ),
+    items: z.array(
+        z.object({
+            id: z.number(),
+            title: z.string(),
+            order: z.number(),
+            status: z.string(),
+        })
+    ),
 });
 
 export type InputType = z.infer<typeof UpdateUserBookOrderInput>;
 export type ReturnType = ActionState<InputType, UserBook[]> | undefined;
 
 export const useUpdateUserBookOrder = (options: UseActionOptions = {}) => {
-  const [UpdateUserBookOrder] = useUpdateUserBookOrderMutation();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [UpdateUserBookOrder] = useUpdateUserBookOrderMutation();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const handler = async (input: InputType): Promise<ReturnType> => {
-    setIsLoading(true);
-    try {
-      const { data, errors } = await UpdateUserBookOrder({
-        variables: {
-          data: {
-            items: input.items,
-          },
-        },
-        errorPolicy: 'all',
-      });
+    const handler = async (input: InputType): Promise<ReturnType> => {
+        setIsLoading(true);
+        try {
+            const { data, errors } = await UpdateUserBookOrder({
+                variables: {
+                    data: {
+                        items: input.items,
+                    },
+                },
+                errorPolicy: 'all',
+            });
 
-      if (data && data.updateUserBookOrder && !errors) {
-        options.onSuccess?.(data.updateUserBookOrder);
-        toast({ title: 'Book reordered' });
-      }
-      if (errors) {
-        options.onError?.(errors[0].message);
-        toast({ title: errors[0].message, variant: 'destructive' });
-        return {
-          error: errors[0].message,
-        };
-      }
-    } finally {
-      options.onComplete?.();
-      setIsLoading(false);
-    }
-  };
-  const updateUserBookOrder = createSafeAction(
-    UpdateUserBookOrderInput,
-    handler
-  );
+            if (data && data.updateUserBookOrder && !errors) {
+                options.onSuccess?.(data.updateUserBookOrder);
+                toast({ title: 'Book reordered' });
+            }
+            if (errors) {
+                options.onError?.(errors[0].message);
+                toast({ title: errors[0].message, variant: 'destructive' });
+                return {
+                    error: errors[0].message,
+                };
+            }
+        } finally {
+            options.onComplete?.();
+            setIsLoading(false);
+        }
+    };
+    const updateUserBookOrder = createSafeAction(
+        UpdateUserBookOrderInput,
+        handler
+    );
 
-  return {
-    updateUserBookOrder,
-    isLoading,
-  };
+    return {
+        updateUserBookOrder,
+        isLoading,
+    };
 };
