86177bbeb5edb037d925318d3e5730f5c40e2e67
Vivian Lin <linvivian61@gmail.com>
2024-03-15 11:59:47 -0400

    fix list card

--- apps/web/src/modules/bookshelves/components/column-header.tsx
+++ apps/web/src/modules/bookshelves/components/column-header.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import { ColumnOptions } from './column-options';
 
 interface ColumnHeaderProps {
-  title: string;
-  totalBooks: number;
+    title: string;
+    totalBooks: number;
 }
 
 export const ColumnHeader: React.FC<ColumnHeaderProps> = ({
-  title,
-  totalBooks,
+    title,
+    totalBooks,
 }) => {
-  return (
-    <div className='items-start- flex justify-between gap-x-2 px-2 pt-2 text-sm font-semibold'>
-      <div
-        // onClick={enableEditing}
-        className='h-7 w-full border-transparent px-2.5 py-1 font-medium'
-      >
-        {title}
-      </div>
-      <div className='h-auto w-auto'>{totalBooks}</div>
-      <ColumnOptions
-      // onAddCard={onAddCard}
-      // data={data}
-      />
-    </div>
-  );
+    return (
+        <div className='items-center flex justify-between gap-x-2 px-2 pt-2 text-sm font-semibold'>
+            <div
+                // onClick={enableEditing}
+                className='h-7 w-full border-transparent px-2.5 py-1 font-medium'
+            >
+                {title}
+            </div>
+            <div className='h-auto w-auto py-1 items-center'>{totalBooks}</div>
+            <ColumnOptions
+            // onAddCard={onAddCard}
+            // data={data}
+            />
+        </div>
+    );
 };
 export default ColumnHeader;
