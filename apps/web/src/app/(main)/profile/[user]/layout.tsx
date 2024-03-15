3436c52f936dad09ed41ebb3069339861eb1f94c
Vivian Lin <linvivian61@gmail.com>
2024-03-15 11:29:58 -0400

    profile page responsive

--- apps/web/src/app/(main)/profile/[user]/layout.tsx
+++ apps/web/src/app/(main)/profile/[user]/layout.tsx
@@ -1,14 +1,14 @@
 import { getCurrentUser } from '@/lib/auth';
 interface ProfileLayoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default async function ProfileLayout({ children }: ProfileLayoutProps) {
-  const user = await getCurrentUser();
-  return (
-    <div className='mt-5 grid w-full items-center'>
-      {/* <UserNav items={siteConfig.userNav} user={user} /> */}
-      <div className='flex-col justify-center'>{children}</div>
-    </div>
-  );
+    const user = await getCurrentUser();
+    return (
+        <div className='mt-5 grid w-full items-center overflow-x-auto'>
+            {/* <UserNav items={siteConfig.userNav} user={user} /> */}
+            <div className='flex-col justify-center'>{children}</div>
+        </div>
+    );
 }
