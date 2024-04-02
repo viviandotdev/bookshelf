1be99e5dfeef28baa6b0b0c074a3bec91608e8b8
Vivian Lin <linvivian61@gmail.com>
2024-04-02 15:06:38 -0400

    fix close upload

--- apps/web/src/modules/settings/components/upload-file-dialog.tsx
+++ apps/web/src/modules/settings/components/upload-file-dialog.tsx
@@ -59,7 +59,7 @@ export default function UploadFileDialog({
   };
 
   return (
-    <Dialog open={open}>
+    <Dialog>
       <DialogTrigger asChild>
         <Button
           variant='secondary'
