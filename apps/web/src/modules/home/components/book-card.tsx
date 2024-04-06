d8e019d0364909bba52b8239e9af8fb770040b6e
Vivian Lin <linvivian61@gmail.com>
2024-04-06 15:40:43 -0400

    update currently reading card

--- apps/web/src/modules/home/components/book-card.tsx
+++ apps/web/src/modules/home/components/book-card.tsx
@@ -9,11 +9,12 @@ import EditBookCardMenu from './edit-book-card-menu';
 import LogBookButton from '@/modules/layout/components/log-book-button';
 import useLogBookModal from '@/components/modals/log-book-modal/use-log-book-modal';
 import { Button, buttonVariants } from '@/components/ui/button';
-import { cn, getCoverUrl } from '@/lib/utils';
+import { cn, formatAuthors, getCoverUrl } from '@/lib/utils';
 import { useJournalEntryModal } from '@/components/modals/journal-entry-modal/use-journal-entry-modal';
 import useUserBookStore from '@/stores/use-user-book-store';
 
 import BookActions from '@/components/book-actions';
+import Link from 'next/link';
 
 interface ReadingCardProps {
   userBook: UserBook;
@@ -36,25 +37,51 @@ export const BookCard: React.FC<ReadingCardProps> = ({
   const { book, shelves } = userBook;
   return (
     <Card
-      className='group/item w-full cursor-pointer'
+      className='group/item w-full'
       onMouseLeave={() => {
         setOpenDropdown(false);
       }}
     >
       <CardContent className='relative p-2.5'>
         <div className='flex space-x-4'>
-          <BookCover src={getCoverUrl(book, Size.Small)} size={'xs'} />
-          <div className='flex flex-col justify-center '>
+          <Link
+            href={`/book/${book?.id}`}
+            className={'text-beige hover:text-stone-500'}
+          >
+            <BookCover src={getCoverUrl(book!, Size.Small)} size={'xs'} />
+          </Link>
+
+          <div className='flex w-full flex-col justify-center '>
             <div className='flex flex-col'>
-              <h3 className='line-clamp-1 text-base font-medium text-gray-900'>
-                {book?.title}
+              <h3 className='line-clamp-2 text-sm font-medium leading-4 text-gray-900'>
+                <Link href={`/book/${book?.id}`} className={'hover:underline'}>
+                  {book?.title}
+                </Link>
               </h3>
-              <p className='text-sm text-gray-600'>
-                {book?.authors && book?.authors[0].name}
+              <p className='mt-1 text-xs text-gray-600'>
+                {formatAuthors(book!)}
               </p>
-              <p className='flex items-center gap-1 text-sm text-gray-500'>
-                <Icons.read className='h-4 w-4 text-gray-400' />
-                Finished
+              <p className='mt-1 flex items-center gap-1.5 '>
+                {/* <Icons.read className='h-4 w-4 text-gray-400' /> */}
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setBook(book!);
+                    updateStatus(status);
+                    updateBookId(book!.id);
+                    journalEntryModal.onOpen();
+                  }}
+                  variant='pill'
+                  size='sm'
+                  className=' h-5 rounded-sm p-2 text-[9px]'
+                >
+                  Update Progress
+                </Button>
+
+                <span className=' text-[10px] text-gray-600'>•</span>
+                <span className=' items-center text-[11px] font-medium'>
+                  75%
+                </span>
               </p>
               <div
                 className={cn(
@@ -63,22 +90,6 @@ export const BookCard: React.FC<ReadingCardProps> = ({
                 )}
               >
                 <div className='absolute right-2 top-2 flex rounded-md shadow-md'>
-                  {cardStatus == 'Currently Reading' && (
-                    <Button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setBook(book!);
-                        updateStatus(status);
-                        updateBookId(book!.id);
-                        journalEntryModal.onOpen();
-                      }}
-                      variant={'card'}
-                      size={'xs'}
-                      className={cn('rounded-l-md px-2')}
-                    >
-                      <Icons.plus className={`h-4 w-4`} />
-                    </Button>
-                  )}
                   <BookActions
                     book={book!}
                     setOpenAlert={setOpenAlert}
