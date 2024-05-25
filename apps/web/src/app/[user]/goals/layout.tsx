562ce949d2c373cd8d49a2675d72eaf1bb266696
Vivian Lin <linvivian61@gmail.com>
2024-05-25 06:35:59 -0400

    feat(web): remove secondary header

--- apps/web/src/app/[user]/goals/layout.tsx
+++ apps/web/src/app/[user]/goals/layout.tsx
@@ -1,7 +1,6 @@
 import { SiteFooter } from '@/modules/layout/templates/site-footer';
 import { getCurrentUser } from '@/lib/auth';
 import SiteHeader from '@/modules/layout/templates/site-header';
-import SecondaryHeader from '@/modules/layout/components/secondary-header';
 
 interface PageLayoutProps {
   children: React.ReactNode;
@@ -10,26 +9,9 @@ interface PageLayoutProps {
 
 export default async function PageLayout({ children }: PageLayoutProps) {
   const user = await getCurrentUser();
-  // pathname
-  const subItems = [
-    {
-      title: 'Activity',
-      href: user ? `/book/activity` : '/',
-    },
-    {
-      title: 'Journal',
-      href: user ? `/book/journal` : '/',
-    },
-    {
-      title: 'Lists',
-      href: user ? `/book/lists` : '/',
-    },
-  ];
   return (
     <div className='flex min-h-screen flex-col bg-background'>
-      <SiteHeader user={user}>
-        <SecondaryHeader user={user} subItems={subItems} />
-      </SiteHeader>
+      <SiteHeader user={user}></SiteHeader>
 
       <main className='container z-40 mx-auto flex-1 '>{children}</main>
       <SiteFooter />
