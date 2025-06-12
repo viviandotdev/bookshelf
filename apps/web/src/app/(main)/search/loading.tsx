92c3e5f106191f00f9bc3e985988831b47b07fca
vivianlin <linvivian61@gmail.com>
2025-06-11 20:58:05 -0400

    fix: add search loading component

--- apps/web/src/app/(main)/search/loading.tsx
+++ apps/web/src/app/(main)/search/loading.tsx
@@ -1,4 +1,9 @@
 
 export default function Loading() {
-    return <div className='container'></div>;;
+    return (
+        <div className="flex flex-col items-center justify-center min-h-[50vh]">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-beige-700 mb-4"></div>
+            <p className="text-gray-600 text-lg">Loading...</p>
+        </div>
+    );
 }
